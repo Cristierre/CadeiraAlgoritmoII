6e9f30509f183c06fd08160dac97a020a70ac7ec
Cristierre <cristierre.konrath@acad.ftec.com.br>
2019-09-09 20:52:05 -0300

    Começando os ajustes nas eliminações das peças

--- Jogo de damas Trabalho G1.c
+++ Jogo de damas Trabalho G1.c
@@ -15,13 +15,7 @@ int main(){
 	inicioDeJogo(tabuleiro);
 	movePeca(tabuleiro);
 	
-//	for(indiceLinhas = 0 ; indiceLinhas < 8; indiceLinhas++){
-//		printf("%d |",indiceLinhas);
-//		for(indiceColunas = 0 ; indiceColunas < 8; indiceColunas++){
-//			printf("  %d  ", tabuleiro[indiceLinhas][indiceColunas]);
-//		}
-//		printf("\n \n");
-//	}
+
 	printf("\n \n");
 	printf("\n \n");
 
@@ -132,43 +126,65 @@ int movePeca(int tabuleiro[8][8]){
 		scanf("%d", &jogada);
 		if(jogador == 1){
 			if(jogada == 1 && tabuleiro[linhaOrigem][colunaOrigem] == PECA_PRETA){
-					tabuleiro[linhaOrigem][colunaOrigem] = 0;
-					tabuleiro[linhaOrigem-1][colunaOrigem-1] = PECA_PRETA;
-					jogador = 0;
-			}else{		
-				if(jogada == 0 && tabuleiro[linhaOrigem][colunaOrigem] == PECA_PRETA){
-					tabuleiro[linhaOrigem][colunaOrigem] = 0;
-					tabuleiro[linhaOrigem-1][colunaOrigem+1] = PECA_PRETA;
-					jogador = 0;					
-				}else{
-					jogadaInvalida = 1;
-				}			
-			}
-		}else{
-			if(jogador == 0){
-				if(jogada == 1 && tabuleiro[linhaOrigem][colunaOrigem] == PECA_BRANCA){
-					tabuleiro[linhaOrigem][colunaOrigem] = 0;
-					tabuleiro[linhaOrigem+1][colunaOrigem-1] = PECA_BRANCA;	
-					jogador = 1;		
-			}else{
-				if(jogada == 0 && tabuleiro[linhaOrigem][colunaOrigem] == PECA_BRANCA){
-					tabuleiro[linhaOrigem][colunaOrigem] = 0;
-					tabuleiro[linhaOrigem+1][colunaOrigem+1] = PECA_BRANCA;
-					jogador = 1;
+					
+					if(	tabuleiro[linhaOrigem-1][colunaOrigem-1] == 0){
+						tabuleiro[linhaOrigem][colunaOrigem] = 0;
+						tabuleiro[linhaOrigem-1][colunaOrigem-1] = PECA_PRETA;
+						jogador = 0;
+						jogadaInvalida = 0;						
+					}else{
+						if(tabuleiro[linhaOrigem-1][colunaOrigem-1] == PECA_BRANCA && tabuleiro[linhaOrigem-2][colunaOrigem-2] == 0){
+							tabuleiro[linhaOrigem][colunaOrigem] = 0;
+							tabuleiro[linhaOrigem-2][colunaOrigem-2] = PECA_PRETA;
+							jogador = 0;	
+							jogadaInvalida = 0;						
+						}else{
+							if(jogada == 0 && tabuleiro[linhaOrigem][colunaOrigem] == PECA_PRETA){
+								if(	tabuleiro[linhaOrigem-1][colunaOrigem+1] == 0){
+									tabuleiro[linhaOrigem][colunaOrigem] = 0;
+									tabuleiro[linhaOrigem-1][colunaOrigem+1] = PECA_PRETA;
+									jogador = 0;						
+									jogadaInvalida = 0;	
+								}else{
+									if(	tabuleiro[linhaOrigem-1][colunaOrigem+1] != PECA_BRANCA && tabuleiro[linhaOrigem-1][colunaOrigem+1] == 0){
+										tabuleiro[linhaOrigem][colunaOrigem] = 0;
+										tabuleiro[linhaOrigem-1][colunaOrigem+2] = PECA_PRETA;
+										jogador = 0;
+										jogadaInvalida = 0;						
+									}else{
+										jogadaInvalida = 1;
+									}
+								}
+							}
+						}
+					}
 				}else{
 					jogadaInvalida = 1;
 				}
+		}else{
+			if(jogador == 0){
+				if(jogada == 1 && tabuleiro[linhaOrigem][colunaOrigem] == PECA_BRANCA){
+					if(tabuleiro[linhaOrigem+1][colunaOrigem-1] != PECA_BRANCA &&tabuleiro[linhaOrigem+1][colunaOrigem-1] != PECA_PRETA){
+						tabuleiro[linhaOrigem][colunaOrigem] = 0;
+						tabuleiro[linhaOrigem+1][colunaOrigem-1] = PECA_BRANCA;	// testar destino aqui 
+						jogador = 1;
+						jogadaInvalida = 0;
+					}else{
+						if(tabuleiro[linhaOrigem+1][colunaOrigem-1] == PECA_PRETA && tabuleiro[linhaOrigem+2][colunaOrigem-2] != PECA_PRETA)
+						if(jogada == 0 && tabuleiro[linhaOrigem][colunaOrigem] == PECA_BRANCA){
+							tabuleiro[linhaOrigem][colunaOrigem] = 0;
+							tabuleiro[linhaOrigem+1][colunaOrigem+1] = PECA_BRANCA;
+							jogador = 1;
+						}else{
+					jogadaInvalida = 1;
+					}
+				}	
 			}
-				
 		}
-		if(jogadaInvalida == 1){
-			system("cls");
-			printf("Jogada Invalida \n \n");
-		}
-		return 0;
 	}
-	}	
 }
+}
+
 
 
 
